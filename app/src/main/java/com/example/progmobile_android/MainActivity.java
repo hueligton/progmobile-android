e0bf194fde6c706441a15d19b59beed7e8cbb3ce
hueligton <hueligton@users.noreply.github.com>
2019-05-25 16:01:19 -0400

    ModelFacade -Criado construtor -Alterado assinatura de métodos listEvents e listPurchases para getListEvents e getListPurchases -Redirecinamento para managers -Inclusão de método searchEventByName
    EventManager, PurchaseManager, UserManager
    -Criado classes com métodos a implementar
    
    Repository
    -Criação da classe Repository com padrão Singleton
    
    Manifest
    -Incluído autorizações para tráfego de dados via internet, inclusive para Android Pie
    
    network_security_config
    -Configurado autorização para acesso da plain text no Android Pie
    
    build.gradle
    -Inclusão das bibliotecas Volley e GSon
    
    Signed-off-by: hueligton <hueligton@users.noreply.github.com>

--- app/src/main/java/com/example/progmobile_android/MainActivity.java
+++ app/src/main/java/com/example/progmobile_android/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.progmobile_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.progmobile_android.model.ModelFacade;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.test);
+
+        new ModelFacade(this).getListEvents();
+
     }
 }
---
+++ app/src/main/java/com/example/progmobile_android/model/repository/Repository.java
@@ -0,0 +1,60 @@
+package com.example.progmobile_android.model.repository;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class Repository {
+    private static Repository instance;
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+    private static Context ctx;
+
+    private Repository(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized Repository getInstance(Context context) {
+        if (instance == null) {
+            instance = new Repository(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+}

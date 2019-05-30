8f2929ea902e63325e72b969b6569a7f4988339d
Felipe Caggi <felipecaggi@gmail.com>
2019-05-30 19:08:56 -0400

    Implementação da view Home

---
+++ app/src/main/java/com/example/progmobile_android/view/Home.java
@@ -0,0 +1,64 @@
+package com.example.progmobile_android.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.progmobile_android.R;
+
+public class Home extends AppCompatActivity
+{
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.action_login:
+                // startActivity(new Intent(this, Login.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void buyTickets(View view)
+    {
+        // startActivity(new Intent(this, ListTickets.class));
+    }
+
+    public void showMyTickets(View view)
+    {
+        // startActivity(new Intent(this, MyTickets.class));
+    }
+}

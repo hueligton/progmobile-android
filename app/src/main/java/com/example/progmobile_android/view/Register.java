3a99c8303d2fe3f6b5dc7a673a1b21f0a9a2df54
Felipe Caggi <felipecaggi@gmail.com>
2019-05-30 22:28:40 -0400

    Implementação da view Register

--- app/src/main/java/com/example/progmobile_android/view/Register.java
+++ app/src/main/java/com/example/progmobile_android/view/Register.java
@@ -7,20 +7,20 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.progmobile_android.R;
 import com.example.progmobile_android.model.ManagerFacade;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     private ManagerFacade managerFacade;
 
-    private Button btLogin;
+    private EditText etName;
+    private EditText etEmail;
     private EditText etLogin;
     private EditText etPassword;
 
@@ -31,7 +31,7 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,51 +44,57 @@ public class Login extends AppCompatActivity {
     }
 
     private void captureViewComponents() {
-        btLogin = findViewById(R.id.btLogin);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
         etLogin = findViewById(R.id.etLogin);
         etPassword = findViewById(R.id.etPassword);
     }
 
-    public void login(View view) {
+    public void register(View view) {
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
         String login = etLogin.getText().toString();
         String password = etPassword.getText().toString();
 
-        final boolean[] loginValidation = {false};
+        final boolean[] registerValidation = {false};
 
-        if(validateFields(login, password)) {
-            managerFacade.login(login, password, object -> {
-                loginValidation[0] = true;
+        if(validateFields(name, email, login, password)) {
+
+            managerFacade.createUser(login, name, password, email, object -> {
+                registerValidation[0] = true;
             });
 
-            if (!loginValidation[0]) {
-                Toast.makeText(this, "Login e/ou senha invalido, verifique os dados preenchidos",
-                        Toast.LENGTH_SHORT).show();
+            if (!registerValidation[0]) {
+                Toast.makeText(Register.this, "Não conseguimos realizar seu " +
+                        "cadastro no momento, tente novamente mais tarde!", Toast.LENGTH_SHORT)
+                        .show();
 
+                etName.getText().clear();
+                etEmail.getText().clear();
                 etLogin.getText().clear();
                 etPassword.getText().clear();
             }
         }
 
-        if (loginValidation[0]) {
-            startActivity(new Intent(this, Home.class));
+        if (registerValidation[0]) {
+            Toast.makeText(Register.this, "Cadastro realizado com sucesso",
+                    Toast.LENGTH_SHORT).show();
+
+            startActivity(new Intent(this, Login.class));
         }
 
     }
 
-    private boolean validateFields(String login, String password) {
+    private boolean validateFields(String name, String email, String login, String password) {
         Boolean result = true;
 
-        if(login.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Por favor, insira um login e uma senha",
+        if(name.isEmpty() || email.isEmpty() || login.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Por favor, preencha todos os campos",
                     Toast.LENGTH_SHORT).show();
             result = false;
         }
 
         return result;
-    }
-
-    public void register(View view) {
-        startActivity(new Intent(this, Register.class));
     }
 
 }

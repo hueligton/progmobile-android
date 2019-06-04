35f4e9ae802770c1a6b1075181dcc85bde52b758
Felipe <felipecaggi@gmail.com>
2019-06-03 20:29:01 -0400

    Implementação da view PurchaseDetails
    (Entity) Purchase
    - TODO implementation of  getTickets()

---
+++ app/src/main/java/com/example/progmobile_android/view/RecyclerAdapter/RATicketType3.java
@@ -0,0 +1,55 @@
+package com.example.progmobile_android.view.RecyclerAdapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.progmobile_android.R;
+import com.example.progmobile_android.model.entities.Ticket;
+
+import java.util.List;
+
+public class RATicketType3 extends RecyclerView.Adapter<RATicketType3.ViewHolder> {
+
+    private List<Ticket> list;
+
+    public RATicketType3(List<Ticket> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.comp_ticket_type_3,
+                viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Ticket ticket = list.get(i);
+        viewHolder.tvTicketPrice.setText(ticket.getTicketType().getType());
+        viewHolder.tvTicketType.setText(String.format("%s", ticket.getTicketType().getPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvTicketPrice;
+        private TextView tvTicketType;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTicketPrice = itemView.findViewById(R.id.tvTicketPrice);
+            tvTicketType = itemView.findViewById(R.id.tvTicketType);
+        }
+    }
+}
